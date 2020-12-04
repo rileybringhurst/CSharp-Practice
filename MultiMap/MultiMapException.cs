3d119157271984c98d21aabfd82c17bbe1db0542
rileybringhurst <60456576+rileybringhurst@users.noreply.github.com>
2020-12-04 03:10:14 -0700

    Add files via upload

---
+++ MultiMap/MultiMapException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MM.Util
+{
+    public class MultiMapException : Exception
+    {
+
+        public MultiMapException() : base() { }
+
+        public MultiMapException(string exMessage):base(exMessage){}
+
+        public MultiMapException(Exception ExceptionParam, string ExMessage) : base(ExMessage, ExceptionParam) { }
+
+    }
+}
